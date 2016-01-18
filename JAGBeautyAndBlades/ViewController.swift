e14ffe05f2ade05dfa3d04f0486c19d7cc258a15
Abraham Brovold <abrahambrovold@kellys-macbook-pro.local>
2016-01-18 12:11:55 -0600

    Initial Commit

---
+++ JAGBeautyAndBlades/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JAGBeautyAndBlades
+//
+//  Created by Abraham Brovold on 1/18/16.
+//  Copyright © 2016 Hill Country Angel Network. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
