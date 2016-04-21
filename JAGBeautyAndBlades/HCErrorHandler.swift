f4608a92fa5307c7152529a17378126e2413ff6e
Abraham <abraham@jagformen.com>
2016-04-21 07:43:16 -0500

    Build is broken on device. seems to be a code signing issue

---
+++ JAGBeautyAndBlades/HCErrorHandler.swift
@@ -0,0 +1,13 @@
+//
+//  HCErrorHandler.swift
+//  JAGForMen
+//
+//  Created by Abraham Brovold on 4/21/16.
+//  Copyright © 2016 Hill Country Angel Network. All rights reserved.
+//
+
+import UIKit
+
+class HCErrorHandler: NSObject {
+
+}
